97683cb8797cf953231d48111965b1642ac624d8
Leon <iamleonkigen@gmail.com>
2023-10-10 12:52:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 
 // Write your code here
 
-const num1 = 31;
-const num2 = 2;
+let num1 = 31;
+let num2 = 2;
 
 const multiply = num1 * num2;
 
-const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
-const random = generateRandomNumber();
+const random = Math.floor(Math.random() * 100) + 1;
 
-const mod = 17 % 13;
+const num3 = 17;
+const num4 = 13;
+const mod = num3 % num4;
 
 const numberSet = [5, 10, 20, 15, 8];
 const max = Math.max(...numberSet);
 
-console.log('Multiply:', multiply);
-console.log('Random:', random);
+console.log('Result of multiplication:', multiply);
+console.log('Random number:', random);
 console.log('Modulus:', mod);
-console.log('Max:', max);
+console.log('Maximum in the number set:', max);
+
 
 
 
